4c7f9aebd1333eb3188d8cd67035e9d0000e0e75
T4TU <markkanen.tarmo@gmail.com>
2018-04-11 20:55:58 +0300

    Add World

--- 2DEngine/src/me/markkanen/tarmo/environment/World.java
+++ 2DEngine/src/me/markkanen/tarmo/environment/World.java
@@ -1,4 +1,12 @@
 package me.markkanen.tarmo.environment;
 
+import java.awt.Graphics2D;
+
 public class World {
+	
+	public void tick() {
+	}
+	
+	public void render(Graphics2D g) {
+	}
 }
--- 2DEngine/src/me/markkanen/tarmo/gamestates/TestState.java
+++ 2DEngine/src/me/markkanen/tarmo/gamestates/TestState.java
@@ -2,17 +2,28 @@ package me.markkanen.tarmo.gamestates;
 
 import java.awt.Graphics2D;
 
+import me.markkanen.tarmo.environment.World;
+
 public class TestState extends GameState {
+	
+	private World world;
 	
 	public TestState(String name) {
 		super(name);
+		world = new World();
 	}
 
 	@Override
 	public void tick() {
+		world.tick();
 	}
 
 	@Override
 	public void render(Graphics2D g) {
+		world.render(g);
+	}
+	
+	public World getWorld() {
+		return world;
 	}
 }
